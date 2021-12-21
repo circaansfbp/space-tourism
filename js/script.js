afe04b711fd396a524d9abaa916fcfa389fce1ea
circaansfbp <franciscambp@outlook.com>
2021-12-21 18:18:38 -0300

    finishes tech page layout, starts work on slide functionality

--- js/script.js
+++ js/script.js
@@ -6,6 +6,38 @@ const btnMobile = document.querySelector('.btn--mobile-nav');
 const tabsContainer = document.querySelector('.dest-tabs');
 const tabs = document.querySelectorAll('.dest-tab');
 const tabsContent = document.querySelectorAll('.main-dest--img-info');
+const slideNumbersContainer = document.querySelector('.main-tech--tabs-box');
+const slideNumberBtns = document.querySelectorAll('.tech-tab');
+const slidesContent = document.querySelectorAll('.main-tech--info-img-box');
+
+// Slider function
+const slider = function (slides, axis) {
+
+    // Positions the slides upwards (in the Y axis)
+    const hideSlides = function (slds) {
+        slds.forEach((s, i) =>
+            s.style.transform = `translate${axis}(-${100 * i}%)`
+        );
+    }
+
+    // Go to a certain slide (according to numbers)
+    const goToSlide = function (desc) {
+
+    }
+
+    // Initialization 
+    const init = function () {
+        hideSlides(slides);
+    }
+
+    init();
+}
+
+slider(slidesContent, 'Y');
+
+
+
+
 
 // Event listeners
 // Toggle mobile navigation
@@ -27,3 +59,21 @@ tabsContainer?.addEventListener('click', function (e) {
     document.querySelector(`.info-${clickedTab.dataset.tab}`).classList.add('active-tab--info');
 });
 
+// Sliders
+slideNumbersContainer?.addEventListener('click', function (e) {
+    // Guard clause
+    if (!e.target.classList.contains('tech-tab')) return;
+
+    let currSlide = document.querySelector('.tech-tab--active').dataset.slide;
+
+    // Adding active class to clicked button
+    slideNumberBtns.forEach(btn => btn.classList.remove('tech-tab--active'));
+    e.target.classList.add('tech-tab--active');
+
+    // Going to slide
+    document.querySelector(`.slide--${currSlide}`)
+        .style.transform = 'translateY(-100%)';
+
+    document.querySelector(`.slide--${e.target.dataset.slide}`)
+        .style.transform = 'translateY(0)';
+});
